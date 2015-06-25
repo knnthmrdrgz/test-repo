f058a2e6b54c5039b565b8761647c91e9c7963a4
knnthmrdrgz <knnthmrdrgz@gmail.com>
2015-06-25 14:13:47 -0400

    added a print statement
    from the macbook air in the yung MLC

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
-print "this is a test file for test-philes"
+print "this is a test file for test-philes" #edited on the yung github
+print "this test-phile is low on test" #edited on the yung macbook air
